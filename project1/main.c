9a450e4ec3ca40fc24781a6d03fdf454d1e0424a
abdullah <eng.abdullah.alkhadhir@gmail.com>
2024-07-29 15:33:49 +0300

    frist add readme

---
+++ project1/main.c
@@ -0,0 +1,21 @@
+/*
+ * project1.c
+ *
+ * Created: 29/07/2024 15:19:04
+ * Author : 999ab
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
